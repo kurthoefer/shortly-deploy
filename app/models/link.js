6e9be9347753e3654db8fce17c7cc788c21a9264
MKS-Student <mks.student@hackreactor.com>
2017-03-02 21:15:21 -0800

    Implement functionality to pass all tests using Mongoose

--- app/models/link.js
+++ app/models/link.js
@@ -1,19 +1,26 @@
 var db = require('../config');
+var mongoose = require('mongoose');
 var crypto = require('crypto');
 
-var Link = db.Model.extend({
-  tableName: 'urls',
-  hasTimestamps: true,
-  defaults: {
-    visits: 0
-  },
-  initialize: function() {
-    this.on('creating', function(model, attrs, options) {
-      var shasum = crypto.createHash('sha1');
-      shasum.update(model.get('url'));
-      model.set('code', shasum.digest('hex').slice(0, 5));
-    });
-  }
+var urlSchema = mongoose.Schema({
+  url: String,
+  baseUrl: String,
+  code: String,
+  title: String,
+  visits: Number
 });
+
+var createCode = function(url) {
+  var shasum = crypto.createHash('sha1');
+  shasum.update(url);
+  return shasum.digest('hex').slice(0,5);
+};
+
+urlSchema.pre('save', function(next){
+  this.code = createCode(this.url);
+  next();
+});
+
+var Link = mongoose.model('Link', urlSchema);
 
 module.exports = Link;
